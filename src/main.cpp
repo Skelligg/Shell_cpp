e72316bf58eb42633e6a812f3d578b4f82a8a265
michael <michael.lukyanov@student.kdg.be>
2025-11-26 23:54:24 +0100

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  std::cout << "$ ";
-
   std::string command;
-  std::getline(std::cin, command);
 
-  std::cout << command <<": command not found" << '\n';
+  while (command != "exit") {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    std::cout << "$ ";
+
+    std::getline(std::cin, command);
+
+    std::cout << command <<": command not found" << '\n';
+  }
+
 }
